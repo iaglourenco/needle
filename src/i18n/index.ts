3493436207616863382cb1df622273bf11bb96e8
Iago Lourenço <iagojlourenco@gmail.com>
2026-07-27 22:17:03 -0300

    feat: add vue-i18n instance and pt-BR/en locale files

---
+++ src/i18n/index.ts
@@ -0,0 +1,13 @@
+import { createI18n } from "vue-i18n";
+import ptBR from "../locales/pt-BR.json";
+import en from "../locales/en.json";
+
+export const i18n = createI18n({
+  legacy: false,
+  locale: "pt-BR",
+  fallbackLocale: "pt-BR",
+  messages: {
+    "pt-BR": ptBR,
+    en,
+  },
+});
